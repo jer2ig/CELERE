998d34f50c68b1f45505dc0e5b09e74a3979996a
jh1521 <jh1521@ic.ac.uk>
2022-08-28 20:17:19 +0100

    Undo automatic false replacement 2

--- ClassDetect.py
+++ ClassDetect.py
@@ -36,7 +36,7 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.save_dir.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.save_dir.relpath(ROOT, Path.cwd()))  # relative
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
