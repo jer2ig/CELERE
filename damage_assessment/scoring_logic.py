f53c6ce6d3aedb6ff5ed2e542c9324e3ef148559
Julius Herzig <jh1521@ic.ac.uk>
2022-08-31 01:25:44 +0100

    Removed prints

--- damage_assessment/scoring_logic.py
+++ damage_assessment/scoring_logic.py
@@ -66,21 +66,16 @@ def building_scoring_det(walls_damage):
     return Damage.AMBER
 
 def evaluate_wall(wall, damages, model_d):
-    print(damages)
     damages=damages[0]
     if len(damages) ==0:
         return 0
-    print(damages)
-    print("____________")
     percent_dam = 0
     for *xyxy, conf, cls in reversed(damages):
         cls = int(cls)
-        print(model_d.names[cls])
         if model_d.names[cls] == 'rebar':
             return 2
         if model_d.names[cls] != 'superficial':
             percent_dam += iow(xyxy, wall)
-    print(percent_dam)
     if percent_dam > 0.5:
         return 2
     if percent_dam < 0.01:
